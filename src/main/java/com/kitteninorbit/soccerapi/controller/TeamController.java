52f98f9b03398e2329fd626c0177b31e7b10c629
luna <lunasenun@gmail.com>
2026-09-26 15:01:03 -0300

    add team REST controller

---
+++ src/main/java/com/kitteninorbit/soccerapi/controller/TeamController.java
@@ -0,0 +1,25 @@
+package com.kitteninorbit.soccerapi.controller;
+
+import com.kitteninorbit.soccerapi.model.Team;
+import com.kitteninorbit.soccerapi.repository.TeamRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/teams")
+public class TeamController {
+
+    private final TeamRepository teamRepository;
+
+    public TeamController(TeamRepository teamRepository) {
+        this.teamRepository = teamRepository;
+    }
+
+    @GetMapping
+    public List<Team> getAllTeams() {
+        return teamRepository.findAll();
+    }
+}
